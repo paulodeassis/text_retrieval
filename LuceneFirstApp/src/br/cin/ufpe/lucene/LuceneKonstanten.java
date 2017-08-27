eaff88da2dfe422c5c39fb3e633fd1ec75ac088f
paulodeassis <paulodeassis.nascimento@gmail.com>
2017-08-26 21:35:47 -0300

    first commit

---
+++ LuceneFirstApp/src/br/cin/ufpe/lucene/LuceneKonstanten.java
@@ -0,0 +1,8 @@
+package br.cin.ufpe.lucene;
+
+public class LuceneKonstanten {
+	public static final String CONTENTS = "contents";
+	public static final String FILE_NAME = "filename";
+	public static final String FILE_PATH = "filepath";
+	public static final int MAX_SEARCH = 10;
+}
